3d2b4a2e5a28483ba0495963bd74fa5281ca4d36
andresalbab <andres_aalba@hotmail.com>
2023-08-02 13:07:56 -0500

    Add github pages config

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
-  site: 'https://creativetimofficial.github.io',
-  base: '/'
+  site: 'https://andresalbab.github.io',
+  base: '/astro-simple-ecommerce'
 });
